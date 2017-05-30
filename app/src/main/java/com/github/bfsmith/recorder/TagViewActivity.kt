5a716aceda78b57430c24d7d1f21f73dd4ec14f6
Smith, Blake (bs557j) <bs557j@att.com>
2017-05-30 14:04:47 -0500

    Adding a chart for the data

--- app/src/main/java/com/github/bfsmith/recorder/TagViewActivity.kt
+++ app/src/main/java/com/github/bfsmith/recorder/TagViewActivity.kt
@@ -1,16 +1,28 @@
 package com.github.bfsmith.recorder
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.github.bfsmith.recorder.util.MyListAdapter
 import com.github.bfsmith.recorder.util.TemplateRenderer
+import com.github.bfsmith.recorder.util.lineChart
 import com.github.bfsmith.recorder.util.toString
 import org.jetbrains.anko.*
+
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TagViewActivity : AppCompatActivity(), AnkoLogger {
     lateinit var records: List<Record>
     lateinit var tag: Tag
+    lateinit var data: List<Double>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,26 +53,43 @@ class TagViewActivity : AppCompatActivity(), AnkoLogger {
 class TagViewActivityUi : AnkoComponent<TagViewActivity> {
     private val dateFormat = "yyyy-MM-dd HH:mm:ss"
 
-    override fun createView(ui: AnkoContext<TagViewActivity>): View = with(ui) {
-        val listAdapter = MyListAdapter(owner.records, { it.id }) {
-            record ->
-            TemplateRenderer(owner) {
-                with(it) {
-                    linearLayout {
-                        textView {
-                            text = record.date.toString(dateFormat)
-                            textSize = 12f
-                        }
+    fun createLineDataSet(records: List<Record>): LineDataSet {
+        val time = Date().time / 1000
+        val lineDataSet = LineDataSet(records.mapIndexed {
+            index, r ->
+            Entry((time - (86_400 * index)).toFloat(), r.value.toFloat()) //
+        }.reversed(), "Data")
+        lineDataSet.circleRadius = 3f
+        lineDataSet.setDrawCircleHole(false)
+        lineDataSet.valueTextSize = 12f
+        lineDataSet.lineWidth = 2f
+        lineDataSet.color = Color.RED
 
-                        textView {
-                            text = record.value.toString()
-                            textSize = 12f
-                            horizontalPadding = dip(16)
-                        }
-                    }
-                }
-            }
-        }
+        return lineDataSet
+    }
+
+    override fun createView(ui: AnkoContext<TagViewActivity>): View = with(ui) {
+
+
+//        val listAdapter = MyListAdapter(owner.records, { it.id }) {
+//            record ->
+//            TemplateRenderer(owner) {
+//                with(it) {
+//                    linearLayout {
+//                        textView {
+//                            text = record.date.toString(dateFormat)
+//                            textSize = 12f
+//                        }
+//
+//                        textView {
+//                            text = record.value.toString()
+//                            textSize = 12f
+//                            horizontalPadding = dip(16)
+//                        }
+//                    }
+//                }
+//            }
+//        }
 
         verticalLayout {
             padding = dip(8)
@@ -70,12 +99,27 @@ class TagViewActivityUi : AnkoComponent<TagViewActivity> {
                 textSize = 16f
             }
 
-            listView {
-                adapter = listAdapter
+//            listView {
+//                adapter = listAdapter
+//            }.lparams {
+//                margin = dip(8)
+//                width = matchParent
+//                height = wrapContent
+//            }
+
+            lineChart {
+                description.isEnabled = false
+                setTouchEnabled(true)
+                isDragEnabled = true
+                setScaleEnabled(true)
+                setPinchZoom(true)
+                data = LineData(createLineDataSet(owner.records))
+                xAxis.valueFormatter = IAxisValueFormatter { value, axis ->
+                    SimpleDateFormat("MMM d").format(Date(value.toLong()*1000))
+                }
             }.lparams {
-                margin = dip(8)
-                width = matchParent
                 height = matchParent
+                width = matchParent
             }
         }
     }
--- app/src/main/java/com/github/bfsmith/recorder/util/ViewUtils.kt
+++ app/src/main/java/com/github/bfsmith/recorder/util/ViewUtils.kt
@@ -2,7 +2,11 @@ package com.github.bfsmith.recorder.util
 
 import android.content.Context
 import android.view.View
+import android.view.ViewManager
 import android.view.inputmethod.InputMethodManager
+import com.github.mikephil.charting.charts.LineChart
+import org.jetbrains.anko.AnkoViewDslMarker
+import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.sdk25.coroutines.onFocusChange
 
 fun View.showKeyboardOnFocus(ctx: Context) {
@@ -14,4 +18,8 @@ fun View.showKeyboardOnFocus(ctx: Context) {
             inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
         }
     }
+}
+
+inline fun ViewManager.lineChart(theme: Int = 0, init: (@AnkoViewDslMarker LineChart).() -> Unit): LineChart {
+    return ankoView({ ctx -> LineChart(ctx) }, theme) { init() }
 }
